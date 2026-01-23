e672f3f20f908eb345df69162d39720a4f22da78
Hassan5573 <hassan.britishcouncil2000@gmail.com>
2026-01-23 16:33:24 -0700

    every thing is okay

--- python1.py
+++ python1.py
@@ -1,6 +1,15 @@
 
-z = input("enter your name")
+z = input("enter your name ")
 if z == "Hassan":
   print("that's great brother ")
 else:
   print ("that's not the true answer ", 3+4 )
+
+i = 7
+while i > 3 :
+  print("i love sudan")
+  i = i - 1 
+
+
+print(1+23)
+
