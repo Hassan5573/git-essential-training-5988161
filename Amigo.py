21310dd763f6b14056e9c9b8fe0cc439602dc2f4
Hassan5573 <hassan.britishcouncil2000@gmail.com>
2026-01-23 16:16:14 -0700

    after changes

--- Amigo.py
+++ Amigo.py
@@ -1,3 +1,11 @@
 x = 54
 y = 4 
-print("the final answer is :" x+y ")
+print("the final answer is :", x+y )
+z = input('tell me about your day brother ')
+if z == "the full story":
+  print("wrong answer nigga")
+elif z == "laies":
+  print("tell us more about this story . that doesn't sound like the true story ")
+else:
+  print("you're fired try apply for another role in deffirent company ")
+
